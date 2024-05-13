a24004d95cdf6fedb3fa78990d012c775483ec68
oladeji anthony <oladejianthony98@gmail.com>
2024-05-13 12:09:48 +0100

    update

---
+++ server/index.js
@@ -0,0 +1,7 @@
+//Here we are importing all the dependencies that we have installed
+
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose"
+import cors from "cors"
+
